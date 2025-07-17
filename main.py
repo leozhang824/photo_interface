bd18107e6a62658af31032b284af1ce6e62622a1
LeotheDumbo <leo8024zhang@gmail.com>
2025-07-17 13:56:03 -0500

    removed older ver

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 pg = st.navigation(
   [
-    st.Page("visual.py", title="Photosynthesis Data Visualizer - Regular"),
-    st.Page("predict_model.py", title="Prediction Model - Regular"),
+    # st.Page("visual.py", title="Photosynthesis Data Visualizer - Regular"),
+    # st.Page("predict_model.py", title="Prediction Model - Regular"),
     # For microcontroller data
     st.Page("visual_micro.py", title="Photosynthesis Data Visualizer - MicroBit"),
     st.Page("predict_model_micro.py", title="Prediction Model - MicroBit"),
